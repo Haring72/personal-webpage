f8b7bb38f6e2e1c60524de727b25a4998ed2282d
Haring72 <megalan7210@gmail.com>
2026-07-31 11:21:46 +0200

    Header component created

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import ContactLink from "./ContactLink";
+
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <img></img>
+        <div>Nombre Empresa</div>
+      </div>
+
+      <nav></nav>
+    </header>
+  );
+}
