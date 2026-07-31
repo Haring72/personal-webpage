e146450f39237f7bf6af6be3857b61284b5d8dcb
Haring72 <megalan7210@gmail.com>
2026-07-31 12:47:10 +0200

    Footer component created and filled with testing data

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer>
+      <span>Nombre Empresa</span>
+      <span>© {new Date().getFullYear()} · Todos los derechos reservados</span>
+    </footer>
+  );
+}
