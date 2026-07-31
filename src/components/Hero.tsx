e89fa631e73bd6b455cbcf1142928fb39063d197
Haring72 <megalan7210@gmail.com>
2026-07-31 12:00:22 +0200

    Hero component created

---
+++ src/components/Hero.tsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+  return (
+    <section>
+      <img></img>
+      <div></div>
+
+      <div>
+        <div>Subnombre 1</div>
+        <h1>
+          Nombre 1<br />
+          <span>Nombre 2</span>
+        </h1>
+        <p>Añadir descripción del sitio web</p>
+        <a>Explorar servicios</a>
+      </div>
+    </section>
+  );
+}
