e58c456e80218cdfef286474c783fc28033715a4
Haring72 <megalan7210@gmail.com>
2026-07-31 11:23:54 +0200

    Header component filled with testing data

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,20 @@ export default function Header() {
         <div>Nombre Empresa</div>
       </div>
 
-      <nav></nav>
+      <nav>
+        <ContactLink href="mailto:test@empresa.com" label="Email">
+          Item 1
+        </ContactLink>
+        <ContactLink href="tel:+1234567890" label="Teléfono">
+          Item 2
+        </ContactLink>
+        <ContactLink href="https://wa.me/1234567890" label="WhatsApp">
+          Item 3
+        </ContactLink>
+        <ContactLink href="https://linkedin.com" label="LinkedIn">
+          Item 4
+        </ContactLink>
+      </nav>
     </header>
   );
 }
